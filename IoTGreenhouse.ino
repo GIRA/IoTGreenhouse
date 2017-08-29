d7e5969a7afba4f2597ed49cbfb51d96a2c84adb
Tera <matias.teragni@gmail.com>
2017-08-29 17:05:59 -0300

    Removed unused analogs. Added average on the remaining. Added temperature calculation. this might need some work

--- IoTGreenhouse.ino
+++ IoTGreenhouse.ino
@@ -2,11 +2,22 @@
 #include <ThingerYun.h>
 #include <Stepper.h>
 #define STEPS 48
+//this is because i have some bug on the temperature calc.
+#define temperatureDif 26
 
-//cables named 4 3 1 ??
+//layout:
+//A0 = LDR
+//A1 & A2 = therm.
+//A3=switch
+//D8 = R2
+//D9 = R1
+//D10 =Fan1
+//D11 =Fan2
+//D13 =onBoardLed
+//Stepper: D3,D4,D5,D6 cables named 4 3 1 ??
 Stepper stepper(STEPS, 3, 4,5,6);
 
-ThingerYun thing("**", "**", "**");
+ThingerYun thing("****", "****", "****");
 bool pin13 = false;
 bool pin8 = false;
 bool pin9 = false;
@@ -14,10 +25,36 @@ bool pin10 = false;
 bool pin11 = false;
 bool stepping=false; 
 
+const int numReadings = 20;
+int current=0;
+int A0readings[numReadings];
+int A1readings[numReadings];
+int A2readings[numReadings];
 
+float A0avg=0.0;
+float A1avg=0.0;
+float A2avg=0.0;
 
+float avg(int* values)
+{
+  float total=0.0;
+   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    total += values[thisReading];
+  }
+  return total/numReadings;
+  
+}
 
-
+double toDegrees(float RawADC) {
+ double Temp;
+ Temp = log(10000.0*((1024.0/RawADC-1)));  //for pull-down configuration
+ //Temp =log(10000.0/(1024.0/RawADC-1)); // for pull-up configuration
+ Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+ Temp = Temp - 273.15;            // Convert Kelvin to Celcius
+ //Temp = (Temp * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
+ 
+ return Temp + temperatureDif;
+}
 
 void setup() {
   Bridge.begin();
@@ -79,35 +116,36 @@ thing["D11"] << [](pson & in) {
     digitalWrite(11, pin11 ? HIGH : LOW);
   };
 
-  thing["D7"] >> [](pson & out) {
-    out = digitalRead(7);
-  };
+ 
   
   thing["A0"] >> [](pson & out) {
-    out = analogRead(A0);
+    out = A0avg;
   };
   thing["A1"] >> [](pson & out) {
-    out = analogRead(A1);
+    out = toDegrees( A1avg);
   };
   thing["A2"] >> [](pson & out) {
-    out = analogRead(A2);
+    out = toDegrees(A2avg);
   };
   thing["A3"] >> [](pson & out) {
-    out = analogRead(A3);
+    out = (analogRead(A3)>512);
   };
-    thing["A4"] >> [](pson & out) {
-    out = analogRead(A4);
-  };
-  thing["A5"] >> [](pson & out) {
-    out = analogRead(A5);
-  };
+  
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  A0readings[current]=analogRead(A0);
+  A1readings[current]=analogRead(A1);
+  A2readings[current]=analogRead(A2);
+  current++;
+  current%=numReadings;
+  
+  A0avg=avg(A0readings);
+  A1avg=avg(A1readings);
+  A2avg=avg(A2readings);
   thing.handle();
   if(stepping ){
     stepper.step(30);
-
     }
 }
